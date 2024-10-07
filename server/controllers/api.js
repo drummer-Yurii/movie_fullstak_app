452cff740fc6b0c705c5b4259c16ee7b0466cec4
Yurii Rybachok <drummer2009@ukr.net>
2024-10-07 10:00:33 +0300

    get all posts controller

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -1,7 +1,14 @@
+const Post = require('../models/posts');
+
 module.exports = class API {
   // Fetch all the posts
   static async fetchAllPost(req, res) {
-    res.send('Hello from API');
+    try {
+      const posts = await Post.find();
+      res.status(200).json(posts);
+    } catch (err) {
+      res.status(404).json({ message: err.message });
+    }
   }
   // Fetch post by ID
   static async fetchPostByID(req, res) {
