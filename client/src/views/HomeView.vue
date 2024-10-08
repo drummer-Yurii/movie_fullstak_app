7f6b060d2b41cc7d2829f32e48ad21cdf84ba3e2
Yurii Rybachok <drummer2009@ukr.net>
2024-10-08 14:43:58 +0300

    create home page & display data from database

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,7 +1,31 @@
 <script setup>
+import { onMounted, ref } from 'vue';
+import API from '../api';
 
+const posts = ref([]);
+
+onMounted(async () => {
+  posts.value = await API.getAllPost();
+});
 </script>
 
 <template>
-  <h1>Home Page</h1>
+  <v-container>
+    <v-row no-gutters>
+      <v-col sm="4" class="pa-3" v-for="post in posts" :key="post._id">
+        <v-card class="pa-1">
+          <v-img height="250" :src="`/${post.image}`"></v-img>
+          <v-btn class="ml-4 mt-3" small outlined color="indigo">
+            {{ post.category }}
+          </v-btn>
+          <v-card-title class="headline">
+            {{ post.title }}
+          </v-card-title>
+          <v-card-text class="py-0">
+            <p>{{ post.content.substring(0, 100) + '...' }}</p>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
