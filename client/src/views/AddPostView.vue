b4ca876065ed24da4a0f5f836f0f7d7a82ea5c41
Yurii Rybachok <drummer2009@ukr.net>
2024-10-08 16:52:41 +0300

    add post view for create post with form

--- client/src/views/AddPostView.vue
+++ client/src/views/AddPostView.vue
@@ -1,7 +1,21 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-    <h1>Add Post Page</h1>
+  <v-container>
+    <v-row no-gutters>
+      <v-col sm="10" class="mx-auto">
+        <v-card class="pa-5">
+          <v-card-title>Add New Post</v-card-title>
+          <v-divider></v-divider>
+          <v-form ref="form" class="pa-5" enctype="multipart/form-data">
+            <v-text-field label="Title" prepend-icon="mdi-note" :rules="rules"></v-text-field>
+            <v-text-field label="Category" prepend-icon="mdi-view-list" :rules="rules"></v-text-field>
+            <v-textarea label="Content" prepend-icon="mdi-note-plus" :rules="rules"></v-textarea>
+            <v-file-input :rules="rules" show-size counter multiple label="Select Image"></v-file-input>
+            <v-btn type="submit" class="mt-3" color="primary">Add Post</v-btn>
+          </v-form>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
